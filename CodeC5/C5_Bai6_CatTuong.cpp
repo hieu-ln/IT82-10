3604ba0a4c8e10187604f24c56a4b437169fc2b6
nct682000 <1851050178tuong@ou.edu.vn>
2019-08-12 22:29:13 +0700

    C5

---
+++ CodeC5/C5_Bai6_CatTuong.cpp
@@ -0,0 +1,273 @@
+#include<iostream>
+#include<stdio.h>
+#include<fstream>
+using namespace std;
+
+#define MAX 20
+int a[MAX];
+int A[MAX][MAX];
+int n;
+int sp;
+int front, rear;
+char vertex[MAX];
+
+///STACk
+void InitStack(int a[], int &sp)
+{
+	sp = -1;
+}
+
+int isEmptyS()
+{
+	if(sp == -1)
+		return 1;
+	return 0;
+}
+
+int PushS(int x)
+{
+	if(sp < MAX -1)
+	{
+		a[++sp] = x;
+		return 1;
+	}
+	return 0;
+}
+
+int PopS(int a[], int &sp, int &x)
+{
+	if(sp!= -1)
+	{
+		x = a[sp--];
+	}
+	return 0;
+}
+
+//QUEUE
+
+void initQueue(int a[], int &front, int &rear)
+{
+	front = -1;
+	rear = -1;
+}
+
+int isEmptyQ(int a[], int rear, int front)
+{
+	if(front == -1)
+		return 1;
+	return 0;
+}
+
+int PushQ(int a[], int &rear, int &front, int x)
+{
+	if( rear - front == MAX -1)
+		return 0;
+	else
+	{
+		if(front == -1)
+			front = 0;
+		if(rear == MAX -1)
+		{
+			for(int i = front; i <= rear; i++)
+				a[i = front] = a[i];
+			rear = MAX - 1 - front;
+			front = 0;
+		}
+		a[++rear]= x;
+		return 1;
+	}
+}
+
+int PopQ(int a[], int &rear,int & front, int &x)
+{
+	if(front == -1)
+		return 0;
+	else
+	{
+		x = a[front++];
+		if(front > rear)
+		{
+			front = -1;
+			rear = -1;
+		}
+		return 1;
+	}
+}
+//end QUEUE
+
+void InitGraph()
+{
+	n = 0;
+}
+
+void inputGraphFromText()
+{
+	string line;
+	ifstream myfile("matranke1.txt");
+	if(myfile.is_open())
+	{
+		myfile>>n;
+		for(int i = 0; i< n; i++)
+			myfile>>vertex[i];
+		for(int i = 0; i < n; i++)
+		{
+			for(int j = 0; j < n; j++)
+				myfile>>A[i][j];
+		}
+	}
+}
+
+
+void inputGraph()
+{
+	cout <<"nhap so dinh do thi n: ";
+	cin>> n;
+	for(int i= 0; i < n; i++)
+	{
+		cout <<"nhap ten dinh: ";
+		cin >> vertex[i];
+		cout <<"nhap vao dong thu " << i + 1<< ":  ";
+		for(int j = 0; j < n; j++)
+			cin >> A[i][j];
+	}
+}
+
+
+void outputGraph()
+{
+	for(int i = 0; i < n; i++)
+	{
+		for(int j = 0; j < n;j++)
+			cout <<A[i][j]<< " ";
+		cout << endl;
+	}
+}
+
+void outPut(int a[], int n)
+{
+	for(int i = 0; i < n; i++)
+		cout << vertex[a[i]]<< " ";
+}
+
+//cau 4.3:duyet do thi theo chieu rong BFS
+int C[100], bfs[100];
+int nbfs = 0;
+void initC()
+{
+	for(int i = 0; i < n; i++)
+		C[i] = 1;
+}
+
+void BFS(int v)
+{
+	int w, p;
+	PushQ( a,rear, front, v);
+	C[v] = 0;
+	while(front != -1)
+	{
+		PopQ(a,rear, front, p);
+		bfs[nbfs] = p;
+		nbfs++;
+		for(w = 0; w < n; w++)
+		{
+			if(C[w] && A[p][w] == 1)
+			{
+				PushQ(a,rear, front, w);
+				C[w] = 0;
+			}
+		}
+	}
+}
+
+//cau 4.4: duyet do thi theo DFS
+int dfs[100];
+int ndfs = 0;
+void DFS(int s)
+{
+	PushS(s);
+	dfs[ndfs] = s;
+	ndfs++;
+	C[s] = 0;
+	int v = -1, u = s;
+	while(isEmptyS() == 0)
+	{
+		if(v == n)
+			PopS( a,sp,u);
+		for(v = 0; v < n;v++)
+			if(A[u][v]!=0 && C[v] == 1)
+			{
+				PushS(u);
+				PushS(v);
+				dfs[ndfs] = v;
+				ndfs++;
+				C[v] = 0;
+				u = v;
+				break;
+
+			}
+	}
+}
+int main()
+{
+	int a[MAX];
+	int b[MAX];
+	int choice, sp, sp_b, x, i;
+	system("cls");
+	cout <<"------------BAI TAP 4, CHUONG 5, DO THI------------" << endl;
+	cout <<"1.khoi tao ma tran ke rong \n";
+	cout <<"2.nhap ma tran ke tu file text \n";
+	cout <<"3.nhap ma tran ke \n";
+	cout <<"4.xuat ma tran ke \n";
+	cout <<"5.duyet do thi theo BFS - DSLK \n" ;
+	cout <<"6.duyet do thi theo dfs - DSLK \n ";
+	cout <<"7.thoat \n" ;
+	do{
+		cout <<"vui long chon so de thuc hien: ";
+		cin>> choice ;
+		switch(choice)
+		{
+		case 1:
+			InitGraph();
+			cout << "ban vua khoi tao ma tran ke rong thanh cong! \n";
+			break;
+		case 2:
+			inputGraphFromText();
+			cout <<"ban vau nhap ma tran ke tu file: \n";
+			outputGraph();
+			break;
+		case 3:
+			inputGraph();
+			break;
+		case 4:
+			outputGraph();
+			break;
+		case 5:
+			initQueue( a, front,rear);
+			initC();
+			cout <<"vui long nhap dinh xuat phat: ";
+			cin >> x;
+			nbfs = 0;
+			BFS(x);
+			cout <<"thu tu dinh sau khia duyet BFS: " << endl;
+			outPut(bfs, n);
+			break;
+		case 6:
+			InitStack( a,sp);
+			initC();
+			cout <<"vui long nhap dnh xuat phat: ";
+			cin >> x;
+			ndfs = 0;
+			DFS(x);
+			cout <<"thu tu dinh sau khia duyet BFS: " << endl;
+			outPut(dfs, n);
+			break;
+		case 7:
+			cout <<"goodbye........!" << endl;
+			break;
+		default:
+			break;
+		}
+	}while(choice !=7);
+	system("pause");
+	return 0;
+}
